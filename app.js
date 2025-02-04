939ea4b870c63e499a0ab4e6cc5ec278f082121d
Mesi14 <Mesi18@proton.me>
2025-02-04 19:17:25 +0200

    :construction: implement the createTask function in app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const taskName = document.getElementById("task-name");
 const taskDescript = document.getElementById("task-descript");
 const addTask = document.getElementById("button-addon2");
 const myTasks = document.getElementById("my-tasks");
+const currTask = document.getElementById("task-list");
 
-console.log(taskName, taskDescript, addTask);
 addTask.addEventListener("click", () => {
-  createTask();
-  console.log(createTask);
+  const newTask = createTask(taskName.value, taskDescript.value);
+  console.log(createTask(taskName.value, taskDescript.value));
+
+  addTodo(newTask);
+  console.log(todos);
 })
 
 const createElement = (itemType, itemClass, itemTxt = null) => {
@@ -16,8 +19,32 @@ const createElement = (itemType, itemClass, itemTxt = null) => {
   return item;
 }
 
+const createTask = (name, val) => {
+  const liItem = createElement("li", "list-item d-flex flex-row");
+  const taskDescript = createElement("div", "task-description");
+  const chckbox = createElement("input", "is-done");
+  chckbox.setAttribute = ("type", "checkbox");
+  const headerLiEle = createElement("h6", "header-ele");
+  const paragraphEle = createElement("p", "paragraphEle");
+  headerLiEle.innerHTML = name;
+  paragraphEle.innerHTML = val;
+  taskDescript.appendChild(chckbox);
+  taskDescript.appendChild(headerLiEle);
+  taskDescript.appendChild(paragraphEle);
+  
+  const iconDiv = createElement("div", "icon-div");
+  const editIcon = createElement("i", "bi bi-pencil");
+  const deleteIcon = createElement("i", "bi bi-trash");
+  iconDiv.appendChild(editIcon);
+  iconDiv.appendChild(deleteIcon);
+
+  liItem.appendChild(taskDescript);
+  liItem.appendChild(iconDiv);
+  return liItem;
+}
+
 let todos = [];
-const addTodo = () => {
-  todos.push({taskName: taskDescript})
-  console.log(todos);
+const addTodo = (currTask) => {
+  todos.push(currTask)
+  return todos;
 }
